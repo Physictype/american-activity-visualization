c35402dafcc4bad2da16f019f642472fb0ddb559
Physictype <87732115+Physictype@users.noreply.github.com>
2024-11-12 13:48:22 -0800

    SPEEEED

--- script.js
+++ script.js
@@ -15,16 +15,16 @@ function weightedRandom(items, weights) {
 		}
 	}
 }
-var speed = 600
+var speed = 600;
 function createChart(width, data, groups, transitionData, invalidation) {
 	const height = width;
 	const color = d3.interpolateWarm;
 	const canvas = document.createElement("canvas");
 	canvas.width = width;
 	canvas.height = height;
+	const svg = d3.select("svg");
 	const context = canvas.getContext("2d");
 	const nodes = data.map((d) => Object.create(d));
-  
 
 	const simulation = d3
 		.forceSimulation(nodes)
@@ -53,7 +53,16 @@ function createChart(width, data, groups, transitionData, invalidation) {
 		)
 		// .force("charge",d3.forceCenter(width / 2, height / 2))
 		.on("tick", ticked);
-
+		var paused = false;
+	// simulation.stop();
+	document.getElementById("pauseplay").addEventListener("click",() => {
+		if (paused) {
+			simulation.restart();
+		} else {
+			simulation.stop();
+		}
+		paused = !paused
+	})
 	// Add event listeners to canvas
 	// d3.select(canvas)
 	//     .on("touchmove", event => event.preventDefault())
@@ -67,17 +76,21 @@ function createChart(width, data, groups, transitionData, invalidation) {
 	}
 	var fiveminute = 0;
 	function changeNodeById(id) {
-    // console.log(transitionData[fiveminute]);
+		if (paused) {
+			return;
+		}
+		// console.log("oh gosh");
+		// console.log(transitionData[fiveminute]);
 		// console.log(transitionData.map(row => row[0]));
 		let newGroup = weightedRandom(
-			[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,15,16,17,18],
+			[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
 			transitionData[fiveminute][nodes[id].group]
 		);
-    if (typeof newGroup === "undefined") {
-      // console.log(nodes[id],transitionData[fiveminute][nodes[id].group])
-      return;
-    }
-    nodes[id].group = newGroup;
+		if (typeof newGroup === "undefined") {
+			// console.log(nodes[id],transitionData[fiveminute][nodes[id].group])
+			return;
+		}
+		nodes[id].group = newGroup;
 		// console.log(nodes[randomNode].group)
 		simulation
 			.force(
@@ -85,12 +98,12 @@ function createChart(width, data, groups, transitionData, invalidation) {
 				d3
 					.forceX()
 					.x((d) => {
-            if (typeof groups[d.group] == 'undefined') {
-              console.log(d.group);
-              return 10000;
-            }
-            return groups[d.group].x
-          })
+						if (typeof groups[d.group] == "undefined") {
+							// console.log(d.group);
+							return 10000;
+						}
+						return groups[d.group].x;
+					})
 					.strength(0.01)
 			)
 			.force(
@@ -98,64 +111,102 @@ function createChart(width, data, groups, transitionData, invalidation) {
 				d3
 					.forceY()
 					.y((d) => {
-            if (typeof groups[d.group] == 'undefined') {
-              console.log(d.group);
-              return 10000;
-            }
-            return groups[d.group].y
-          })
+						if (typeof groups[d.group] == "undefined") {
+							// console.log(d.group);
+							return 10000;
+						}
+						return groups[d.group].y;
+					})
 					.strength(0.01)
-			)
+			);
 		simulation.alpha(1).restart();
 	}
-  let changeTimes = [];
-  function setRecurringChange(id) {
-    let newTime = 5*60*1000/speed*Math.random();
-    changeNodeById(id);
-    setInterval(() => {
-      setRecurringChange(id)
-    },newTime+5*60*1000/speed-changeTimes[id]);
-    changeTimes[id] = newTime;
-  }
-  for (let i = 0; i < data.length; i++) {
-    let time = 5*60*1000/speed*Math.random();
-    changeTimes[i] = time;
-    setInterval(() => {
-      setRecurringChange(i);
-    },time)
-  }
-	setInterval(() => {
-		fiveminute += 1;
-		fiveminute = fiveminute % 288;
-    console.log(fiveminute)
-	}, 5*60*1000/speed);
-  let minute = 0;
-  setInterval(() => {
-    minute+=1;
-    document.getElementById("time").innerText=((minute-(minute %60))/60).toString().padStart(2, '0')+":"+(minute % 60).toString().padStart(2, '0'); 
-    console.log("hi")
-  },60*1000/speed)
-
+	let changeTimes = [];
+	function setRecurringChange(id) {
+		let newTime = ((5 * 60 * 1000) /speed) * Math.random();
+		changeNodeById(id);
+		setTimeout(() => {
+			setRecurringChange(id);
+		}, newTime + (5 * 60 * 1000) /speed - changeTimes[id]);
+		changeTimes[id] = newTime;
+	}
+	for (let i = 0; i < data.length; i++) {
+		let time = ((5 * 60 * 1000) /speed) * Math.random();
+		changeTimes[i] = time;
+		setTimeout(() => {
+			setRecurringChange(i);
+		}, time);
+	}
+	let minute = 0;
+	function setFiveMinuteInterval() {
+		setTimeout(() => {
+			fiveminute += 1;
+			fiveminute = fiveminute % 288;
+			setFiveMinuteInterval();
+		}, (5 * 60 * 1000) / speed);
+	}
+	function setMinuteInterval() {
+		setTimeout(() => {
+			minute += 1;
+			document.getElementById("time").innerText =
+				((minute - (minute % 60)) / 60).toString().padStart(2, "0") +
+				":" +
+				(minute % 60).toString().padStart(2, "0");
+			setMinuteInterval();
+		}, (60 * 1000) /speed);
+	}
+	setFiveMinuteInterval();
+	setMinuteInterval();
+	document.getElementById("speedSlider").addEventListener("change",(e) => {
+		speed = parseFloat(document.getElementById("speedSlider").value);
+		console.log((60 * 1000) /speed);
+	})
+	
+		// console.log("hi");
+	
+	var x = d3.scaleLinear([-500,500],[0,1000])
+	var y = d3.scaleLinear([-500,500],[0,1000])
+	svg.selectAll("circle")
+		.data(nodes)
+		.enter() // This creates a new circle for each data point
+		.append("circle")
+		.attr("cx", (d) => x(d.x)) // Set the x-coordinate (cx) of the circle
+		.attr("cy", (d) => y(d.y)) // Set the y-coordinate (cy) of the circle
+		.attr("r", (d) => d.r) // Set the radius (r) of the circle
+		.attr("fill", "steelblue")
+		.on('mouseover', function (d, i) {
+			d3.select(this).transition()
+				 .duration(50)
+				 .attr('opacity', '.5')})
+		.on('mouseout', function (d, i) {
+		d3.select(this).transition()
+				.duration(50)
+				.attr('opacity', '1')});
 	function ticked() {
-		context.clearRect(0, 0, width, height);
-		context.save();
-		context.translate(width / 2, height / 2);
-		for (let i = 0; i < nodes.length; ++i) {
-			const d = nodes[i];
-			context.beginPath();
-      
-			context.moveTo(d.x + d.r, d.y);
-			context.arc(d.x, d.y, d.r, 0, 2 * Math.PI);
-			context.fillStyle = color(d.group / 14);
-			context.fill();
-			// console.log(d.x,d.y)
-		}
-		// context.fillStyle = "black";
-		// for (let i = 0; i < groups.length; i++) {
+		svg.selectAll("circle") // This creates a new circle for each data point
+			.attr("cx", (d) => x(d.x)) // Set the x-coordinate (cx) of the circle
+			.attr("cy", (d) => y(d.y)) // Set the y-coordinate (cy) of the circle
+			.attr("r", (d) => d.r) // Set the radius (r) of the circle
+			.attr("fill", (d) => color(d.group / 14));
+		// context.clearRect(0, 0, width, height);
+		// context.save();
+		// context.translate(width / 2, height / 2);
+		// for (let i = 0; i < nodes.length; ++i) {
+		// 	const d = nodes[i];
+		// 	context.beginPath();
 
-		//   context.fillText(transitionData[i].Activity,groups[i].x*1.3,groups[i].y*1.3);
+		// 	context.moveTo(d.x + d.r, d.y);
+		// 	context.arc(d.x, d.y, d.r, 0, 2 * Math.PI);
+		// 	context.fillStyle = color(d.group / 14);
+		// 	context.fill();
+		// 	// console.log(d.x,d.y)
 		// }
-		context.restore();
+		// // context.fillStyle = "black";
+		// // for (let i = 0; i < groups.length; i++) {
+
+		// //   context.fillText(transitionData[i].Activity,groups[i].x*1.3,groups[i].y*1.3);
+		// // }
+		// context.restore();
 	}
 	return canvas;
 }
@@ -174,11 +225,11 @@ const data = [];
 for (let i = 0; i < 400; i++) {
 	data.push({
 		group: Math.floor(Math.random() * 15),
-		r: 2,
+		r: 4,
 	});
 }
 
 // Assuming 'invalidation' is a Promise
 const invalidation = Promise.resolve(); // Replace with actual invalidation logic
-const chart = createChart(700, data, groups,timeData,invalidation);
+const chart = createChart(700, data, groups, timeData, invalidation);
 document.body.appendChild(chart);
